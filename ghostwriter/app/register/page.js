202ceeab35363a020fec49eb9d6d66d46fc71f7b
Kyung-min-Kim <kyungminkim0901@gmail.com>
2023-08-22 21:51:12 +0900

    회원가입 및 레이아웃
    1. 레이아웃
     - 레이아웃 틀 및 global.css
    
    2. 회원가입(resiger 디렉토리)
     - 회원가입 page.js와 register.css
    
    3. 폰트 및 이미지 추가 (public - fonts 및 images)
    
    4. util database.js는 mongodb 수정 필요

---
+++ ghostwriter/app/register/page.js
@@ -0,0 +1,39 @@
+import "../globals.css";
+import styles from "./register.css";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Register() {
+  return (
+    <div className={styles.container}>
+      <Link href="/">
+        <Image
+          src="/images/Ghostwriter.svg"
+          alt="logo_image"
+          width={250}
+          height={250}
+          className="logo"
+        />
+      </Link>
+
+      <div className="register_box">
+        <h4>CREATE ACCOUNT</h4>
+        <form method="POST" action="/api/auth/signup">
+          <label htmlFor="name">Ghostname</label>
+          <input id="name" name="name" type="text" />
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" type="text" />
+          <label htmlFor="password">Password</label>
+          <input id="password" name="password" type="password" />
+          <label htmlFor="confirm_password">Confirm password</label>
+          <input
+            id="confirm_password"
+            name="confirm_password"
+            type="password"
+          />
+          <button type="submit">JOIN</button>
+        </form>
+      </div>
+    </div>
+  );
+}
